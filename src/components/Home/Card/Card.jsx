83d6d1b68073c7104add1ce19a6eb64df2675e93
reachmohdaltaf <reachmohdaltaf@gmail.com>
2024-11-20 02:42:43 +0530

    new

--- src/components/Home/Card/Card.jsx
+++ src/components/Home/Card/Card.jsx
@@ -7,15 +7,15 @@ function Card({ title, link1, link2, link3, link4, pages }) {
     <div className="flex flex-col gap-5 bg-white p-5 w-full md:w-[48%] lg:w-[23%] relative top-0 md:top-[350px]">
       {/* Title */}
       <div className="h-15">
-        <h3 className="text-base md:text-sm  font-semibold">{title}</h3>
+        <h3 className="  h-[100px]  text-2xl font-semibold">{title}</h3>
       </div>
       {/* Images Section */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 h-4/5">
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-3  h-2/5">
         <div className="flex flex-col items-center">
           <img
             src={link1}
             alt="Item 1"
-            className="h-[70px] w-[70px] md:h-[100px] md:w-[100px]"
+            className="h-[80px] w-[80px] md:h-[120px] md:w-[150px]"
           />
           <p className="text-xs md:text-sm text-center">
             Cushion covers, bedsheets & more
@@ -25,7 +25,7 @@ function Card({ title, link1, link2, link3, link4, pages }) {
           <img
             src={link2}
             alt="Item 2"
-            className="h-[70px] w-[70px] md:h-[100px] md:w-[100px]"
+            className="h-[70px] w-[70px] md:h-[120px] md:w-[150px]"
           />
           <p className="text-xs md:text-sm text-center">
             Figurines, vases & more
@@ -35,7 +35,7 @@ function Card({ title, link1, link2, link3, link4, pages }) {
           <img
             src={link3}
             alt="Item 3"
-            className="h-[70px] w-[70px] md:h-[100px] md:w-[100px]"
+            className="h-[70px] w-[70px] md:h-[120px] md:w-[150px]"
           />
           <p className="text-xs md:text-sm text-center">
             Cushions, bedsheets & more
@@ -45,7 +45,7 @@ function Card({ title, link1, link2, link3, link4, pages }) {
           <img
             src={link4}
             alt="Item 4"
-            className="h-[70px] w-[70px] md:h-[100px] md:w-[100px]"
+            className="h-[70px] w-[70px] md:h-[120px] md:w-[150px]"
           />
           <p className="text-xs md:text-sm text-center">
             Cushions, bedsheets & more
